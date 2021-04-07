d9f187f67d8633c62afc6a4bca2b556389df2508
Victor <47831936+VictorEgret@users.noreply.github.com>
2021-04-07 12:35:01 +0200

    Compatibility & minor improvements
    - birthdaySeparator fonction improved
    - Support for null information soon
    - Graphical improvements

--- passGenerator.py
+++ passGenerator.py
@@ -1,9 +1,9 @@
 import itertools as it
 import time  
 import os
-  
+
 start_time = time.time()  
-infos = {"Name": "", "Surname": "", "Birthday": ""}
+infos = {"Name": "null", "Surname": "null", "Birthday": "null"}
 
 def saveToTxt(liste: list, filepath: str, filename: str):
     """Save a list to a txt file
@@ -28,7 +28,8 @@ def askInfos(liste: dict):
             givenInfo = input(f"Enter info : {info} (DD/MM/YYYY)\n")
         else:
             givenInfo = input(f"Enter info : {info}\n")
-        liste[info] = givenInfo
+        if not givenInfo == '':
+            liste[info] = givenInfo
 
 def allStrings(string: str):
     """Generate a list of upper and lower possibility of a given string
@@ -51,11 +52,12 @@ def birthdaySeparator(birthday: str):
     Returns:
         (list): day, month and year of birth
     """
-    birthdayList = list(birthday)
-    day = birthdayList[0] + birthdayList[1]
-    month = birthdayList[3] + birthdayList[4]
-    year = birthdayList[6] + birthdayList[7] + birthdayList[8] + birthdayList[9]
-    return day, month, year
+    if len(birthday) == 10:
+        day = birthday[0:2]
+        month = birthday[3:5]
+        year = birthday[6:10]
+        return day, month, year
+    return "null"
 
 def passwords(infos: dict):
     """Generate passwords from the given infos
@@ -68,29 +70,29 @@ def passwords(infos: dict):
     """
     passwordsList = []
     for info in infos:
-        if not info == "Birthday":
-            for x in range(len(allStrings(infos[info]))):
-                passwordsList.append(allStrings(infos[info])[x])                    
-        else:
-            # Name + Surname & Surname + Name
-            for noms in allStrings(infos["Name"]):
+        if not infos[info] == "null":
+            if not info == "Birthday":
+                for x in range(len(allStrings(infos[info]))):
+                    passwordsList.append(allStrings(infos[info])[x])                    
+            else:
+                # Name + Surname & Surname + Name
+                for noms in allStrings(infos["Name"]):
+                    for prenom in allStrings(infos["Surname"]):
+                        nomprenom = noms + prenom
+                        prenomnom = prenom + noms
+                        passwordsList.append(prenomnom)
+                        passwordsList.append(nomprenom)
+                # Name + birthday
+                for noms in allStrings(infos["Name"]):
+                    noms = noms + birthdaySeparator(infos[info])[2]
+                    passwordsList.append(noms)
+                # Surname + birthday  
                 for prenom in allStrings(infos["Surname"]):
-                    nomprenom = noms + prenom
-                    prenomnom = prenom + noms
-                    passwordsList.append(prenomnom)
-                    passwordsList.append(nomprenom)
-            # Name + birthday
-            for noms in allStrings(infos["Name"]):
-                noms = noms + birthdaySeparator(infos[info])[2]
-                passwordsList.append(noms)
-            # Surname + birthday  
-            for prenom in allStrings(infos["Surname"]):
-                prenom = prenom + birthdaySeparator(infos[info])[2]
-                passwordsList.append(prenom)    
-                 
+                    prenom = prenom + birthdaySeparator(infos[info])[2]
+                    passwordsList.append(prenom)    
+                    
     return passwordsList        
-            
-askInfos(infos)
+      
 print("""
     ____  ____     ____                  ______         
    / __ \/  _/    / __ \____ ___________/ ____/__  ____ 
@@ -98,13 +100,16 @@ print("""
  / ____// /_____/ ____/ /_/ (__  |__  ) /_/ /  __/ / / /
 /_/   /___/    /_/    \__,_/____/____/\____/\___/_/ /_/ 
                                                         """)
-
-interval = time.time() - start_time  
-print(f'\nTotal time in seconds: {interval}')
+            
+askInfos(infos)
 
 filename = input("Enter name of the output file:\n")
-path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + "\\passLists\\"
-if not os.path.exists(path):
-    os.makedirs(path)
+
+path = ''
+if os.name == "nt":
+    path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + "\\passLists\\"
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 saveToTxt(passwords(infos), path, filename)
+print(f'\nTotal time: {time.time() - start_time} seconds')
